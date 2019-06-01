74c8118c8f6b289ab595a53163ff7452e31631b1
CNLHC <2463765697@qq.com>
2019-06-01 14:42:03 +0800

    添加Material-UI样式表支持

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,18 @@ module.exports = {
     author: `CNLHC`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      // If you want to use styled components, in conjunction to Material-UI, you should:
+      // - Change the injection order
+      // - Add the plugin
+      options: {
+        stylesProvider: {
+          injectFirst: true,
+        },
+      },
+      // 'gatsby-plugin-styled-components',
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-typescript`,
     {
