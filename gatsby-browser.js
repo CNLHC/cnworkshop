969afabb0e38cbb856922756cc1c2494629c5ebd
CNLHC <cn_lhc@qq.com>
2019-12-10 10:26:52 +0800

    fuck the nodejs module system

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,30 +1,11 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+import React from "react"
+import dark from './src/theme/dark'
+import {ThemeProvider} from '@material-ui/styles'
 
-// You can delete this file if you're not using it
-
-// require("prismjs/themes/prism-twilight.css")
-// require(`katex/dist/katex.min.css`)
-// require("prismjs/themes/prism-tomorrow.css")
-
-// const RootWrapper = require("./src/pages/_provider")
-// console.log(11111,RootWrapper)
-// exports.wrapRootElement =  RootWrapper
-
-const React = require("react")
-const dark  = require('./src/theme/dark')
-const ThemeProvider = require('@material-ui/styles').ThemeProvider
-
-exports.wrapRootElement = ({ element }) => {
+export const wrapRootElement = ({ element }) => {
   return (
     <ThemeProvider theme={dark}>
-      <div id="foo">
         {element}
-      </div>
     </ThemeProvider>
   )
 }
-
